e375aa8d61d69b09a0442d9de6085df8de54e1f9
colinSeven8 <ccnorse80@gmail.com>
2019-11-08 18:25:08 -0700

    Added some 'friends' to the app

--- app/data/friends.js
+++ app/data/friends.js
@@ -16,147 +16,147 @@ let friends = [
         ]
     },
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Ren",
+        "photo": "https://ih0.redbubble.net/image.359543737.4556/poster,840x830,f8f8f8-pad,750x1000,f8f8f8.u5.jpg",
         "scores": [
-            5,
+            4,
+            3,
             1,
+            1,
+            2,
+            1,
+            2,
             4,
             4,
+            3
+        ]
+    },
+    {
+        "name": "Pee Wee",
+        "photo": "https://peewee.com/wp-content/uploads/2014/11/Peewee_about.jpg",
+        "scores": [
+            4,
+            3,
             5,
             1,
             2,
+            1,
+            3,
             5,
+            1,
+            5
+        ]
+    },
+    {
+        "name": "Karen",
+        "photo": "https://i.kym-cdn.com/entries/icons/mobile/000/027/963/karenimg.jpg",
+        "scores": [
             4,
+            4,
+            1,
+            4,
+            4,
+            1,
+            5,
+            5,
+            3,
             1
         ]
     },
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Kyle",
+        "photo": "https://i.kym-cdn.com/entries/icons/original/000/029/943/Screen_Shot_2019-06-04_at_2.41.24_PM.jpg",
+        "scores": [
+            1,
+            4,
+            4,
+            3,
+            5,
+            2,
+            2,
+            4,
+            4,
+            2
+        ]
+    },
+    {
+        "name": "Tom",
+        "photo": "https://purepng.com/public/uploads/large/purepng.com-surprised-tomtom-and-jerrytomjerryanimated-seriesin-1940characters-1701528659755v2ozj.png",
         "scores": [
             5,
+            3,
             1,
             4,
             4,
-            5,
-            1,
             2,
             5,
-            4,
+            3,
+            3,
             1
         ]
     },
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Dick",
+        "photo": "https://media.wired.com/photos/592673ed8d4ebc5ab806a4da/master/pass/richardsimmonsmissing-96436306.jpg",
         "scores": [
-            5,
-            1,
             4,
-            4,
+            3,
+            3,
+            3,
             5,
-            1,
             2,
             5,
-            4,
-            1
+            5,
+            1,
+            5
         ]
     },
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Harry",
+        "photo": "https://image.shutterstock.com/image-photo/london-uk-30-may-2017-260nw-769140469.jpg",
         "scores": [
-            5,
-            1,
             4,
             4,
             5,
-            1,
             2,
             5,
+            1,
+            1,
             4,
-            1
+            4,
+            3
         ]
     },
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Barbara",
+        "photo": "https://parade.com/wp-content/uploads/2019/09/barbara-walters-90-ftr.jpg",
         "scores": [
-            5,
+            4,
+            1,
+            1,
+            1,
+            3,
+            4,
+            3,
             1,
             4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
+            5
         ]
     },
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Marco",
+        "photo": "https://mediaproxy.salon.com/width/1200/height/1200/https://media.salon.com/2018/02/marco_rubio16.jpg",
         "scores": [
+            4,
             5,
+            5,
+            2,
+            2,
             1,
-            4,
-            4,
             5,
             1,
             2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
+            5
         ]
     },
 ]
